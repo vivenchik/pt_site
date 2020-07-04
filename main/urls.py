3dd4377ac29083ce43b198124f16f12e8ed50c2d
Ivan K <vivenchik@gmail.com>
2020-07-04 17:20:30 +0300

    added users auth, logout, identificate group

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from django.contrib.auth.views import auth_login
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('projects/<str:project_name>/', views.project_page, name='project_page'),
-    path('projects/', views.projects_list, name='projects_list'),
-    path('login/', views.login, name='login'),
+    path(r'projects/<str:project_name>/', views.project_page, name='project_page'),
+    path(r'projects/', views.projects_list, name='projects_list'),
+    path(r'login/', views.login, name='login'),
+    path(r'logout/', views.logout, name='logout'),
+    path(r'personal/', views.personal, name='personal'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,12 +3,18 @@ from django.http import HttpResponse
 from .models import Project
 from django.http import Http404
 from django.template import loader
+from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 
+@login_required(login_url='/login/', redirect_field_name='')
 def index(request):
     return render(request, 'index.html')
 
 
+@login_required(login_url='/login/', redirect_field_name='')
 def project_page(request, project_name):
     question = get_object_or_404(Project, project_name=project_name)
     context = {
@@ -17,6 +23,7 @@ def project_page(request, project_name):
     return render(request, 'project.html', context)
 
 
+@login_required(login_url='/login/', redirect_field_name='')
 def projects_list(request):
     projects = Project.objects.order_by('-project_deadline')
     context = {
@@ -26,4 +33,36 @@ def projects_list(request):
 
 
 def login(request):
-    return render(request, 'login.html')
+    submitbutton = request.POST.get("submit")
+    username = ''
+    password = ''
+
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None and user.is_active:
+        dj_login(request, user)
+        return redirect('/')
+    else:
+        # Return an 'invalid login' error message.
+        return render(request, 'login.html')
+
+
+def logout(request):
+    dj_logout(request)
+    return redirect('/login/')
+
+
+@login_required(login_url='/login/', redirect_field_name='')
+def personal(request):
+    context = {
+        'username': request.user.username,
+        'first_name': request.user.first_name,
+        'last_name': request.user.last_name,
+        'email': request.user.email,
+        'groups': request.user.groups.all()[0].name if len(request.user.groups.all()) != 0 else '',
+    }
+    return render(request, 'personal.html', context)
