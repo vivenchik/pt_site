3d53243baa0135021f18e437f6886f0067b5f77b
Ivan K <vivenchik@gmail.com>
2020-07-14 14:15:18 +0300

    #1

--- main/views.py
+++ main/views.py
@@ -9,12 +9,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 
 
-# @login_required(login_url='/login/', redirect_field_name='')
+@login_required(login_url='/login/google-oauth2/', redirect_field_name='')
 def index(request):
     return render(request, 'index.html')
 
 
-# @login_required(login_url='/login/', redirect_field_name='')
+@login_required(login_url='/login/google-oauth2/', redirect_field_name='')
 def project_page(request, project_name):
     question = get_object_or_404(Project, project_name=project_name)
     context = {
@@ -23,7 +23,7 @@ def project_page(request, project_name):
     return render(request, 'project.html', context)
 
 
-# @login_required(login_url='/login/', redirect_field_name='')
+@login_required(login_url='/login/google-oauth2/', redirect_field_name='')
 def projects_list(request):
     projects = Project.objects.order_by('-project_deadline')
     context = {
@@ -32,35 +32,12 @@ def projects_list(request):
     return render(request, 'projects_list.html', context)
 
 
-# def login(request):
-#     submitbutton = request.POST.get("submit")
-#     username = ''
-#     password = ''
-#
-#     form = LoginForm(request.POST or None)
-#     if form.is_valid():
-#         username = form.cleaned_data.get("username")
-#         password = form.cleaned_data.get("password")
-#
-#     user = authenticate(request, username=username, password=password)
-#     if user is not None and user.is_active:
-#         dj_login(request, user)
-#         return redirect('/')
-#     else:
-#         # Return an 'invalid login' error message.
-#         return render(request, 'login.html')
-#
-#
-# def logout(request):
-#     dj_logout(request)
-#     return redirect('/login/')
-
 def logout(request):
     dj_logout(request)
     return redirect('/')
 
 
-# @login_required(login_url='/login/', redirect_field_name='')
+@login_required(login_url='/login/google-oauth2/', redirect_field_name='')
 def personal(request):
     context = {
         'username': request.user.username,
