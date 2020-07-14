93952b139d2c5db1ba8abec0f693d1ba8c631a2b
Ivan K <vivenchik@gmail.com>
2020-07-14 16:25:01 +0300

    #3

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path(r'', views.index, name='index'),
     path(r'projects/<str:project_name>/', views.project_page, name='project_page'),
     path(r'projects/', views.projects_list, name='projects_list'),
     path(r'personal/', views.personal, name='personal'),
-    path('', include('social_django.urls', namespace='social')),
-    path('logout/', views.logout, name='logout'),
+    path(r'', include('social_django.urls', namespace='social')),
+    path(r'logout/', views.logout, name='logout'),
 ]
